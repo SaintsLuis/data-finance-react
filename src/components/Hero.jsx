3b2d6f8e2de9bd87d8cde6f8f80f120e92f0e374
SaintsLuis <santosvluis@outlook.com>
2023-07-20 17:47:43 -0400

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,44 @@
+import TypeIt from 'typeit-react';
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold p-2'>
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
+          Grow with data.
+        </h1>
+
+        <div className='flex justify-center items-center'>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
+            Fast, flexible financing for
+          </p>
+
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'>
+            <TypeIt
+              options={{
+                strings: ['BTB', 'BTC', 'SASS'],
+                speed: 200,
+                breakLines: false,
+                waitUntilVisible: true,
+                autoStart: false,
+                loop: true,
+              }}
+            />
+          </p>
+        </div>
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>
+          Monitor your data analytics to increase revenue for BTB, BTC & SASS
+          platforms.
+        </p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
